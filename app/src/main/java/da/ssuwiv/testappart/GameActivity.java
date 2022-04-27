8d57306c050719a1b2a018c8665f0e3ce6c1bba2
mayb1 <artemsajnev@gmail.com>
2022-04-27 20:27:02 +0300

    New

---
+++ app/src/main/java/da/ssuwiv/testappart/GameActivity.java
@@ -0,0 +1,21 @@
+package da.ssuwiv.testappart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+     //   AnalyticSingleton.getInstance(this).logActivity(this);
+    }
+
+}
